29c19554e589a218c02bf08ce45c8460ed43b151
Viktor Luft <viktor.luft@freiheit.com>
2019-08-11 13:43:48 +0200

    Added docs homepage

---
+++ docs/precache-manifest.07322017351a3a0ec83c2ab5fe3bbfc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aae794c58f8a547d2167fe52821e479",
+    "url": "/react-staged/index.html"
+  },
+  {
+    "revision": "196090bcc73af70ce07c",
+    "url": "/react-staged/static/css/main.14404106.chunk.css"
+  },
+  {
+    "revision": "43b05254b4cbdb56fc87",
+    "url": "/react-staged/static/js/2.31c24a1b.chunk.js"
+  },
+  {
+    "revision": "196090bcc73af70ce07c",
+    "url": "/react-staged/static/js/main.682d981c.chunk.js"
+  },
+  {
+    "revision": "531534d0a9c47b73293e",
+    "url": "/react-staged/static/js/runtime~main.27bee85c.js"
+  }
+]);
