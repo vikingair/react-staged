e02ed5eb34e91dc4b97ae36e0cd934d93b1b4eca
Viktor Luft <viktor.luft@freiheit.com>
2019-09-08 20:54:28 +0200

    Fixed some misbehaviour introduced by babel bug

--- src/staged/useSlides.tsx
+++ src/staged/useSlides.tsx
@@ -2,7 +2,13 @@ import React, { ReactNode, useMemo } from 'react';
 import { modulo } from './util';
 
 export const useSlides = (children: ReactNode[], amount: number, pos: number, paged: number): ReactNode[] => {
-    const amounts = useMemo(() => [...Array(amount * 3)].map((_, i) => i - amount), [amount]);
+    const amounts = useMemo(
+        () =>
+            Array(amount * 3)
+                .fill(undefined)
+                .map((_, i) => i - amount),
+        [amount]
+    );
     return useMemo(
         () =>
             amounts.map(i => (
