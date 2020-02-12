a98d750825551cb221e7b125dbf4a19f8ecb8a09
Viktor Luft <viktor.luft@freiheit.com>
2020-02-12 09:23:00 +0100

    Update docs

---
+++ docs/precache-manifest.9d3b49e534647b3f9ff8cf1d7b8d0775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d586f6d21473d0bcc1dca6706eed6255",
+    "url": "/react-staged/index.html"
+  },
+  {
+    "revision": "6710f4567c6de17e599e",
+    "url": "/react-staged/static/css/main.6ec85224.chunk.css"
+  },
+  {
+    "revision": "dca43bb42f25331c737e",
+    "url": "/react-staged/static/js/2.2dd88aa0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-staged/static/js/2.2dd88aa0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6710f4567c6de17e599e",
+    "url": "/react-staged/static/js/main.563d48e1.chunk.js"
+  },
+  {
+    "revision": "8c470805fd5873657803",
+    "url": "/react-staged/static/js/runtime-main.a85bef83.js"
+  }
+]);
