455b45e5432fd9b6760c5cfc1013183eb07ffe57
Viktor Luft <viktor.luft@freiheit.com>
2021-06-23 15:28:16 +0200

    Update deps and export some type

--- src/staged/index.tsx
+++ src/staged/index.tsx
@@ -1,3 +1,5 @@
 export { Staged } from './Staged';
 export { InfinityStaged } from './InfinityStaged';
 export { useStaged } from './useStaged';
+export type { StagedRef } from './useStaged';
+export type { SlideAnimation } from './css-vars';
