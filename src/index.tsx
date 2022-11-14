0171b6394754e1776f85f8ca38d8eb53930ddde0
Viktor Luft <viktor.luft@freiheit.com>
2022-11-14 08:21:30 +0100

    Update all libs

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,10 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// FIXME: Flickering with concurrent rendering (should get rid of useLayoutEffect and make lib SSR compatible)
+// import { createRoot } from 'react-dom/client';
+//
+// const root = createRoot(document.getElementById('root')!);
+//
+// root.render(<App />);
